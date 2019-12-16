62d2093d7fca65ecc6c6b86fdb1842e7b96d5e6d
Calliyope <erin.joosen@gmail.com>
2019-12-16 12:38:44 +0100

    no message

---
+++ calculator.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.php" media="screen">
+    <title>Price Calculator</title>
+</head>
+
+<body>
+    <div id="home-container">
+    <div id=home-game-container>
+        <span>
+            <h1> Price Calculator </h1>
+        </span>
+
+        <input name="newBlackjack" class="btn btn-primary" type="button" value="Play!" onclick="window.location = './game.php';" />
+        <input class="btn btn-info" type="button" value="Rules" onclick="window.location = './rules.php';" />
+
+        
+</div>
+    </div>
+</body>
+
+</html>
